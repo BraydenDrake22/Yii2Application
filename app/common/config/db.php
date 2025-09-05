5f983bdfb5b2578127e36e70e13488bef8885203
Austin McKean <amckean@breakpoint-labs.com>
2025-09-05 14:27:11 -0400

    Fixes to 403 Forbidden

---
+++ app/common/config/db.php
@@ -0,0 +1,18 @@
+<?php
+$host = getenv('DB_HOST') ?: 'db';
+$port = getenv('DB_PORT') ?: '3306';
+$db = getenv('DB_NAME') ?: 'yii2';
+$user = getenv('DB_USER') ?: 'yii2';
+$pass = getenv('DB_PASSWORD') ?: 'yii2';
+
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => "mysql:host={$host};port={$port};dbname={$db}",
+    'username' => $user,
+    'password' => $pass,
+    'charset' => 'utf8mb4',
+    'attributes' => [
+        PDO::ATTR_TIMEOUT => 3,
+    ],
+];
